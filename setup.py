0a76abef25527c92b470587e8fbb821ffdf70340
lxd <lxd1997xy@163.com>
2023-08-29 17:54:51 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='haplot',
+    version='0.0.1',
+    packages=['haplot'],
+    url='https://github.com/swu1019lab/haplot',
+    license='',
+    author='Xiaodong Li',
+    author_email='lxd1997xy@163.com',
+    description='A set of easy-to-use plot function can be used for haplotype visual analysis'
+)
