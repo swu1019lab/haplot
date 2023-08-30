23510788e4901180eea64f14c6a4b9e3cebe5dea
lxd <lxd1997xy@163.com>
2023-08-30 17:08:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,19 @@ from setuptools import setup
 setup(
     name='haplot',
     version='0.0.1',
-    packages=['haplot'],
+    description='A package for genomic data visualization',
     url='https://github.com/swu1019lab/haplot',
-    license='',
-    author='Xiaodong Li',
+    author='XiaoDong Li',
     author_email='lxd1997xy@163.com',
-    description='A set of easy-to-use plot function can be used for haplotype visual analysis'
+    license='BSD 3-clause',
+    packages=['haplot'],
+    install_requires=[
+        'numpy>=1.22.3',
+        'pandas>=1.4.2',
+        'matplotlib>=3.6.2',
+        'scipy>=1.9.3',
+        'geopandas>=0.13.2'
+    ],
+    zip_safe=False,
+    python_requires='>=3.10'
 )
