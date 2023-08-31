126f05d4c728de389d3dc93b95b513b8b751a896
lxd <lxd1997xy@163.com>
2023-08-31 10:45:19 +0800

    Update test_geomap.py

--- tests/test_geomap.py
+++ tests/test_geomap.py
@@ -10,7 +10,7 @@ import pandas as pd
 from haplot.chart import GeoMapPlot
 
 
-# 随机生成中国地图上的城市坐标
+# generate data
 np.random.seed(0)
 x = np.random.randint(100, 115, 5)
 y = np.random.randint(20, 40, 5)
@@ -24,8 +24,8 @@ data = pd.DataFrame({
     'value3': value3
 })
 
-map_file = "D:\\Bio-script\\codes\\haplot\\china_data.json"
-bound_file = "D:\\Bio-script\\codes\\haplot\\bound_data.geojson"
+map_file = "china_data.json"
+bound_file = "bound_data.geojson"
 
 fig, ax = plt.subplots(figsize=(5, 5))
 GeoMapPlot(data, value_col=[2, 3, 4])
