b7dedd1cf0d95afb4b0080430c4148de7f2fb19d
lxd <lxd1997xy@163.com>
2023-09-02 18:07:10 +0800

    Add networkx

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='haplot',
-    version='0.0.1',
+    version='0.0.2',
     description='A package for genomic data visualization',
     url='https://github.com/swu1019lab/haplot',
     author='XiaoDong Li',
@@ -14,7 +14,8 @@ setup(
         'pandas>=1.4.2',
         'matplotlib>=3.6.2',
         'scipy>=1.9.3',
-        'geopandas>=0.13.2'
+        'geopandas>=0.13.2',
+        'networkx>=3.1',
     ],
     zip_safe=False,
     python_requires='>=3.10'
