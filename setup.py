ea9be8af74809454be4f54bc1c91b525a51d5da9
lxd <lxd1997xy@163.com>
2023-11-08 15:41:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='haplot',
-    version='0.0.3',
+    version='0.0.4',
     description='A package for genomic data visualization',
     url='https://github.com/swu1019lab/haplot',
     author='XiaoDong Li',
